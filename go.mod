0fabc6284fb33b695d76087867024ac1560312ea
Will Confoy <willconfoy@gmail.com>
2024-10-31 21:28:20 -0400

    Minor updates

--- go.mod
+++ go.mod
@@ -3,35 +3,40 @@ module cs498.com/browsersync
 go 1.22.0
 
 require (
-	github.com/go-vgo/robotgo v0.110.1
+	github.com/go-vgo/robotgo v0.110.5
 	github.com/robotn/gohook v0.41.0
-	google.golang.org/grpc v1.64.0
-	google.golang.org/protobuf v1.34.1
+	google.golang.org/grpc v1.67.1
+	google.golang.org/protobuf v1.35.1
 )
 
 require (
-	github.com/gen2brain/shm v0.0.0-20230802011745-f2460f5984f7 // indirect
+	github.com/dblohm7/wingoes v0.0.0-20240820181039-f2b84150679e // indirect
+	github.com/ebitengine/purego v0.8.1 // indirect
+	github.com/gen2brain/shm v0.1.1 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
-	github.com/jezek/xgb v1.1.0 // indirect
-	github.com/kbinani/screenshot v0.0.0-20230812210009-b87d31814237 // indirect
-	github.com/lufia/plan9stats v0.0.0-20230326075908-cb1d2100619a // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	github.com/kbinani/screenshot v0.0.0-20240820160931-a8a2c5d0e191 // indirect
+	github.com/lufia/plan9stats v0.0.0-20240909124753-873cd0166683 // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/otiai10/gosseract v2.2.1+incompatible // indirect
-	github.com/power-devops/perfstat v0.0.0-20221212215047-62379fc7944b // indirect
-	github.com/robotn/xgb v0.0.0-20190912153532-2cb92d044934 // indirect
-	github.com/robotn/xgbutil v0.0.0-20190912154524-c861d6f87770 // indirect
-	github.com/shirou/gopsutil/v3 v3.23.8 // indirect
-	github.com/shoenig/go-m1cpu v0.1.6 // indirect
-	github.com/tklauser/go-sysconf v0.3.12 // indirect
-	github.com/tklauser/numcpus v0.6.1 // indirect
-	github.com/vcaesar/gops v0.30.2 // indirect
-	github.com/vcaesar/imgo v0.40.0 // indirect
+	github.com/otiai10/mint v1.6.3 // indirect
+	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
+	github.com/robotn/xgb v0.10.0 // indirect
+	github.com/robotn/xgbutil v0.10.0 // indirect
+	github.com/shirou/gopsutil/v4 v4.24.9 // indirect
+	github.com/tailscale/win v0.0.0-20241018163102-cfd3289ef17f // indirect
+	github.com/tklauser/go-sysconf v0.3.14 // indirect
+	github.com/tklauser/numcpus v0.9.0 // indirect
+	github.com/vcaesar/gops v0.40.0 // indirect
+	github.com/vcaesar/imgo v0.40.2 // indirect
 	github.com/vcaesar/keycode v0.10.1 // indirect
-	github.com/vcaesar/tt v0.20.0 // indirect
-	github.com/yusufpapurcu/wmi v1.2.3 // indirect
-	golang.org/x/image v0.12.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
+	github.com/vcaesar/tt v0.20.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
+	golang.org/x/image v0.21.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241021214115-324edc3d5d38 // indirect
 )
--- main.go
+++ main.go
@@ -13,152 +13,161 @@ import (
 	node "cs498.com/browsersync/node"
 )
 
-
 var (
-    window string   // window is the string that needs to be in the name of the window for any inputs to be accepted
-    sb strings.Builder
-    pressed map[uint16]bool = make(map[uint16]bool)
-    rawcodedict map[uint16]string = map[uint16]string{
-        162:"l ctrl", 163:"r ctrl", 164:"l alt", 165:"r alt", 91:"win", 160:"l shift", 161:"r shift", 9:"tab", 13:"enter",
-        8:"backspace", 27:"esc", 32:"space"}
-    leader = flag.Bool("l", false, "Whether or not this node is the leader")
-    allowtransfer bool = true
-    addrstring = flag.String("addrs", "localhost:50051", "A space separated list of addresses with ports. Ex: 127.0.0.1:50051")
-	myport = flag.String("port", "50051", "The port for this node to run on")
-    myip = flag.String("myip", "-1", "The ip to run on")
-    thewindow = flag.String("w", "google chrome", "Part of the name of the window ex: google chrome")
+	window      string // window is the string that needs to be in the name of the window for any inputs to be accepted
+	sb          strings.Builder
+	pressed     map[uint16]bool   = make(map[uint16]bool)
+	rawcodedict map[uint16]string = map[uint16]string{
+		162: "l ctrl", 163: "r ctrl", 164: "l alt", 165: "r alt", 91: "win", 160: "l shift", 161: "r shift", 9: "tab", 13: "enter",
+		8: "backspace", 27: "esc", 32: "space"}
+	leader             = flag.Bool("l", false, "Whether or not this node is the leader")
+	allowtransfer bool = true
+	addrstring         = flag.String("addrs", "localhost:50051", "A space separated list of addresses with ports. Ex: 127.0.0.1:50051")
+	myport             = flag.String("port", "50051", "The port for this node to run on")
+	myip               = flag.String("myip", "-1", "The ip to run on")
+	thewindow          = flag.String("w", "google chrome", "Part of the name of the window ex: google chrome")
 )
 
 func main() {
-    flag.Parse()
-    addrs := strings.Split(*addrstring, " ")
-    window = *thewindow  // So this is saying we ignore any inputs not in a window that has "main.go" somewhere in the title. Practically, I'd set this to "google chrome"
-    mynode := node.Start(*leader, window)
-    go mynode.Run(addrs, *myip, *myport) // this is just starting up the node
-    log.Printf("Current PID: %d\n", rb.GetPid())
+	flag.Parse()
+	addrs := strings.Split(*addrstring, " ")
+	window = *thewindow // So this is saying we ignore any inputs not in a window that has "main.go" somewhere in the title. Practically, I'd set this to "google chrome"
+	mynode := node.Start(*leader, window)
+	go mynode.Run(addrs, *myip, *myport) // this is just starting up the node
+	log.Printf("Current PID: %d\n", rb.GetPid())
 
-    handleInputs(window, &mynode)  // we now go into an infinite loop so that we can handle events as the come up
+	handleInputs(window, &mynode) // we now go into an infinite loop so that we can handle events as the come up
 
 }
 
 // This function ensures that we are only sending and receiving inputs that were made in the correct window
 func CheckRightWindow(window string) bool {
-    active := strings.ToLower(rb.GetTitle())
-    log.Println("Current active window: ", active)
-    return strings.Contains(active, window)
+	active := strings.ToLower(rb.GetTitle())
+	log.Println("Current active window: ", active)
+	return strings.Contains(active, window)
 }
-
 
 // This function loops infinitely waiting for keystrokes and mouse events from the user
 func handleInputs(window string, mynode *node.Node) {
-    // TODO: Find a way to ignore inputs from robotgo while paying attention to ones that are from the user
-    // If not possible, stick with leader design like we have here
-    // gohook.AddEvents("q", "ctrl", "shift")
-    if mynode.Leader {
-        fmt.Println("You are the leader")
-    } else {
-        fmt.Println("You are not the leader")
-    }
+	// TODO: Find a way to ignore inputs from robotgo while paying attention to ones that are from the user
+	// If not possible, stick with leader design like we have here
+	// gohook.AddEvents("q", "ctrl", "shift")
+	if mynode.Leader {
+		fmt.Println("You are the leader")
+	} else {
+		fmt.Println("You are not the leader")
+	}
 
-    if allowtransfer {fmt.Println("Anyone can become the leader by pressing f9")}
+	if allowtransfer {
+		fmt.Println("Anyone can become the leader by pressing f9")
+	}
 
-    eventHook := gohook.Start()
-    var e gohook.Event
+	eventHook := gohook.Start()
+	var e gohook.Event
 
-        for e = range eventHook {
+	for e = range eventHook {
 
-            if !mynode.Leader && e.Rawcode != 120 {
-                continue
-            } else if !mynode.Leader && allowtransfer && e.Kind == gohook.KeyUp {
-                log.Println("BECOMING LEADER!!!")
-                log.Println(e)
-                mynode.Leader = true
-                mynode.BroadcastNewLeader()
-                continue
-            }
+		if !mynode.Leader && e.Rawcode != 120 {
+			continue
+		} else if !mynode.Leader && allowtransfer && e.Kind == gohook.KeyUp {
+			log.Println("BECOMING LEADER!!!")
+			log.Println(e)
+			mynode.Leader = true
+			mynode.BroadcastNewLeader()
+			continue
+		}
 
+		if e.Kind == gohook.MouseDown {
+			if !CheckRightWindow(window) {
+				continue
+			}
+			handleClicks(e, mynode)
+		} else if e.Kind == gohook.KeyDown {
+			if pressed[e.Rawcode] || !CheckRightWindow(window) || !*leader {
+				continue
+			}
+			pressed[e.Rawcode] = true
+			handleKeydown(e, mynode)
+		} else if e.Kind == gohook.KeyHold {
+			log.Println(e)
+			if pressed[e.Rawcode] || !CheckRightWindow(window) {
+				continue
+			}
 
-            if e.Kind == gohook.MouseDown {
-                if !CheckRightWindow(window) {continue}
-                handleClicks(e, mynode)
-            } else if e.Kind == gohook.KeyDown {
-                if pressed[e.Rawcode] || !CheckRightWindow(window) || !*leader {continue}
-                pressed[e.Rawcode] = true
-                handleKeydown(e, mynode)
-            } else if e.Kind == gohook.KeyHold {
-                log.Println(e)
-                if pressed[e.Rawcode] || !CheckRightWindow(window) {continue}
+			pressed[e.Rawcode] = true
+			handleKeyholds(e, mynode)
 
-                pressed[e.Rawcode] = true
-                handleKeyholds(e, mynode)
-
-            } else if e.Kind == gohook.KeyUp {
-                if !CheckRightWindow(window) {continue}
-                handleKeyups(e, mynode)
-                pressed[e.Rawcode] = false
-            } else if e.Kind == gohook.MouseWheel {
-                if !CheckRightWindow(window) {continue}
-                handleScrolls(e, mynode)
-            }
-        }
+		} else if e.Kind == gohook.KeyUp {
+			if !CheckRightWindow(window) {
+				continue
+			}
+			handleKeyups(e, mynode)
+			pressed[e.Rawcode] = false
+		} else if e.Kind == gohook.MouseWheel {
+			if !CheckRightWindow(window) {
+				continue
+			}
+			handleScrolls(e, mynode)
+		}
+	}
 }
 
-// 
+//
 // All the functions that start with 'handle' take care of telling the node to send the appropriate signal across the network
-// 
+//
 
 func handleClicks(e gohook.Event, mynode *node.Node) {
-    mynode.SendClick(int(e.Button), int(e.X), int(e.Y))
-    log.Printf("Clicked Button %d -- X: %d, Y: %d\n", e.Button, e.X, e.Y)
-    if e.Button == 3 {
-        log.Println(sb.String())
-    }
+	mynode.SendClick(int(e.Button), int(e.X), int(e.Y))
+	log.Printf("Clicked Button %d -- X: %d, Y: %d\n", e.Button, e.X, e.Y)
+	if e.Button == 3 {
+		log.Println(sb.String())
+	}
 }
 
 func handleKeydown(e gohook.Event, mynode *node.Node) {
 
-    mynode.SendKeyDown(string(e.Keychar))
+	mynode.SendKeyDown(string(e.Keychar))
 
-    sb.WriteString(string(e.Keychar))
-    log.Printf("Pressed key down: %s\n", string(e.Keychar))
+	sb.WriteString(string(e.Keychar))
+	log.Printf("Pressed key down: %s\n", string(e.Keychar))
 }
 
 func handleKeyholds(e gohook.Event, mynode *node.Node) {
-    r, is_in := rawcodedict[e.Rawcode]
-    if is_in {
-        sb.WriteString(r)
-        log.Printf("Key held: %s \n", r)
+	r, is_in := rawcodedict[e.Rawcode]
+	if is_in {
+		sb.WriteString(r)
+		log.Printf("Key held: %s \n", r)
 
-        mynode.SendKeyDown(r)
-    } else {
-        newstr := string(gohook.RawcodetoKeychar(e.Rawcode))
-        sb.WriteString(newstr)
-        log.Printf("Key held: %s \n", newstr)
+		mynode.SendKeyDown(r)
+	} else {
+		newstr := string(gohook.RawcodetoKeychar(e.Rawcode))
+		sb.WriteString(newstr)
+		log.Printf("Key held: %s \n", newstr)
 
-        mynode.SendKeyDown(newstr)
-    }
+		mynode.SendKeyDown(newstr)
+	}
 }
 
 func handleKeyups(e gohook.Event, mynode *node.Node) {
-    r, is_in := rawcodedict[e.Rawcode]
-    if is_in {
-        // sb.WriteString(r)
-        log.Printf("Released key: %s\n", r)
+	r, is_in := rawcodedict[e.Rawcode]
+	if is_in {
+		// sb.WriteString(r)
+		log.Printf("Released key: %s\n", r)
 
-        mynode.SendKeyUp(r)
-    } else {
-        gohook.RawcodetoKeychar(e.Rawcode)
-        newstr := string(gohook.RawcodetoKeychar(e.Rawcode))
-        log.Printf("Released key: %s\n", newstr)
+		mynode.SendKeyUp(r)
+	} else {
+		gohook.RawcodetoKeychar(e.Rawcode)
+		newstr := string(gohook.RawcodetoKeychar(e.Rawcode))
+		log.Printf("Released key: %s\n", newstr)
 
-        mynode.SendKeyUp(newstr)
-    }
+		mynode.SendKeyUp(newstr)
+	}
 }
 
 func handleScrolls(e gohook.Event, mynode *node.Node) {
-    if e.Rotation == 1 {
-        mynode.SendScroll("down")
-    } else {
-        mynode.SendScroll("up")
-    }
+	if e.Rotation == 1 {
+		mynode.SendScroll("down")
+	} else {
+		mynode.SendScroll("up")
+	}
 }
